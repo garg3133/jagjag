2bc8fa773a54da91ca7a724fa517c2a8b9c12909
Priyansh Garg <39924567+garg3133@users.noreply.github.com>
2019-05-15 16:18:42 +0530

    Add files via upload

--- Jagrati new/js/myjs.js
+++ Jagrati new/js/myjs.js
@@ -1,6 +1,9 @@
+//Nav button controls
+
 function nav_button(){
 	var a = document.getElementById("inavbar2");
-	a.style.display="block";
+	a.style.padding="2px 5px 5px";
+	a.style.height="131px";
 	var a = document.getElementById("inav-button-close");
 	a.style.display="block";
 	var a = document.getElementById("inav-button");
@@ -8,7 +11,8 @@ function nav_button(){
 }
 function nav_button_close(){
 	var a = document.getElementById("inavbar2");
-	a.style.display="none";
+	a.style.padding="0";
+	a.style.height="0";
 	var a = document.getElementById("inav-button-close");
 	a.style.display="none";
 	var a = document.getElementById("inav-button");
@@ -16,12 +20,14 @@ function nav_button_close(){
 }
 function for_media_query(x){
 	if (x.matches){
-		document.getElementById("inavbar2").style.display="none";
+		document.getElementById("inavbar2").style.padding="0";
+		document.getElementById("inavbar2").style.height="0px";
 		document.getElementById("inav-button").style.display="block";
 		document.getElementById("inav-button-close").style.display="none";
 	}
 	else{
-		document.getElementById("inavbar2").style.display="none";
+		document.getElementById("inavbar2").style.padding="0";
+		document.getElementById("inavbar2").style.height="0px";
 		document.getElementById("inav-button").style.display="none";
 		document.getElementById("inav-button-close").style.display="none";
 	}
@@ -30,10 +36,11 @@ var x=window.matchMedia("(max-width:760px)");
 for_media_query(x);
 x.addListener(for_media_query);  /*Whenever value of x is changed, for_media_query is called.*/
 
+
 // Navbar closes if clicks anywhere else
 window.onclick = function(e){
 	if(!(e.target.matches('.navbut') || e.target.matches('.menu-icon'))){
-		if(document.getElementById("inavbar2").style.display==="block"){
+		if(document.getElementById("inavbar2").style.height==="131px"){
 			nav_button_close();
 		}
 	}
